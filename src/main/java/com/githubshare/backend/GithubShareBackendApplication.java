105a068f90d91ef8e14c0d445e639f6d1acb60f4
jai <varshitjai@gmail.com>
2025-05-28 07:31:03 +0530

    Initial commit - Spring Boot setup

---
+++ src/main/java/com/githubshare/backend/GithubShareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.githubshare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubShareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubShareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubshare/backend/GithubShareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubshare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubShareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
