4a921f1b538c39861b201c24e37374f02308f822
jai <varshitjai@gmail.com>
2025-06-17 16:43:44 +0530

    Added @Repository annotation

--- src/main/java/com/githubshare/repos/SharedRepoLinkRepository.java
+++ src/main/java/com/githubshare/repos/SharedRepoLinkRepository.java
@@ -5,9 +5,10 @@ import com.githubshare.entity.SharedRepoLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface SharedRepoLinkRepository extends JpaRepository<SharedRepoLink, Long> {
     Optional<SharedRepoLink> findByShareId(String shareId);
     Optional<SharedRepoLink> findByRepoOwner(String repoOwner);
--- src/main/java/com/githubshare/repos/ViewerLinkRepository.java
+++ src/main/java/com/githubshare/repos/ViewerLinkRepository.java
@@ -4,10 +4,11 @@ import com.githubshare.entity.ViewerLink;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface ViewerLinkRepository extends JpaRepository<ViewerLink, Long> {
     Optional<ViewerLink> findByViewerId(String viewerId);
     List<ViewerLink> findByShareId(String shareId);
