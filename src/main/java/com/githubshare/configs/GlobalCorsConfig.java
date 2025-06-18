ff047712980c411bde94b8cc9d63be952737de4f
jai <varshitjai@gmail.com>
2025-06-18 17:49:33 +0530

    upadted cors config

--- src/main/java/com/githubshare/configs/GlobalCorsConfig.java
+++ src/main/java/com/githubshare/configs/GlobalCorsConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class GlobalCorsConfig {
     @Bean
@@ -13,7 +12,10 @@ public class GlobalCorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://github-share-frontend.onrender.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
